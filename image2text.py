96fa8a1cf85c44f02619d8d98ce6a3032fed4fae
liuwons <liuwons@163.com>
2016-05-01 19:56:07 +0800

    add comment.

--- image2text.py
+++ image2text.py
@@ -4,6 +4,7 @@
 import sys
 from PIL import Image
 
+# Convert pixel values to chars.
 def image_to_text(pixels, width, height):
     color = "MNHQ$OC?7>!:-;. "
     string = ""
@@ -14,6 +15,7 @@ def image_to_text(pixels, width, height):
         string += "\n"
     return string
 
+# Load image from file and resize it.
 def load_and_resize_image(imgname, width, height):
     img = Image.open(imgname)
     if img.mode != 'RGB':
@@ -27,6 +29,7 @@ def load_and_resize_image(imgname, width, height):
     img = img.resize((rw, rh), Image.ANTIALIAS)
     return img
 
+# Convert image file to plain ascii text.
 def image_file_to_text(img_file_path, dst_width, dst_height):
     img = load_and_resize_image(img_file_path, dst_width, dst_height)
     pixels = img.load()
